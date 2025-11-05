95b233ff54a5f1bc24cc7b5ac8864ca798a6ca49
mrstrange1708 <junaidsamishaik@gmail.com>
2025-11-05 17:10:04 +0530

    added the backup databases

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 7777;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
