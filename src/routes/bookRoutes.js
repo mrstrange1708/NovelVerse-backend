021f9b89cf6aad4a00855ebb1b7d4939b13b24ba
mrstrange1708 <junaidsamishaik@gmail.com>
2025-11-30 11:51:48 +0530

    changes some basic routes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,12 +5,11 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/books', authMiddleware.authenticateToken, bookController.getAllBooks);
-router.get('/book/:id', authMiddleware.authenticateToken, bookController.getBookById);
+router.get('/book/:slug', authMiddleware.authenticateToken, bookController.getBookBySlug);
 router.post('/createBook', authMiddleware.authenticateToken, bookController.createBook);
 router.post('/book/progress', authMiddleware.authenticateToken, bookController.updateProgress);
 router.put('/updateBook', authMiddleware.authenticateToken, bookController.updateBook);
 router.delete('/deleteBook', authMiddleware.authenticateToken, bookController.deleteBook);
-router.get('/book/slug/:slug', authMiddleware.authenticateToken, bookController.getBookBySlug);
 router.get('/book/:slug/manifest', authMiddleware.authenticateToken, bookController.getManifest);
 
 module.exports = router;
